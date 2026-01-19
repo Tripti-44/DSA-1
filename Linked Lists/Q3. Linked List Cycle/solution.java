27663be4d23e6833f9dad8d69d13502fc8a45f21
Tripti <44tripti@gmail.com>
2026-01-19 14:16:30 +0530

    Add solution for Q3 Linked List Cycle

--- Linked Lists/Q3. Linked List Cycle/solution.java
+++ Linked Lists/Q3. Linked List Cycle/solution.java
@@ -9,12 +9,9 @@
  *     }
  * }
  */
-// Q3 - Linked List Cycle
-// Author: Tripti-44
 public class Solution {
     public boolean hasCycle(ListNode head) {
         ListNode s=head, f=head;
-        //Floyd’s Cycle-Finding Algorithm
         while(f!=null && f.next!=null){
             s=s.next;
             f=f.next.next;
